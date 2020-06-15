c6ea91592821a34d78db26b6b65ec47eed4dc0a8
maojui <maojui0427@gmail.com>
2020-06-16 00:18:24 +0800

    Boring

--- const.py
+++ const.py
@@ -1,12 +1,12 @@
 
 import string
 
-WORD = string.ascii_letters + string.digits + '_'
-SPACE = '\x0c\n\r\t\x0b\xa0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u200a\u2028\u2029\u202f\u205f\u3000\ufeff'
-SYMBOL = '''!@#$%^&*()[]';./,<>?:"{}-=`~|+-\/ '''
 ESCAPE = '{([])^$.|*+?}'
 INESCAPE = '^-'
 LOWER_HEXDIGIT = '0123456789abcdef'
 UPPER_HEXDIGIT = '0123456789ABCDEF'
 CHAR_RANGE = WORD[:-1] # A-Za-z0-9
 CHAR_RANGE_WITH_SYMBOLS = WORD[:-1] + SYMBOL 
+WORD = string.ascii_letters + string.digits + '_'
+SYMBOL = '''!@#$%^&*()[]';./,<>?:"{}-=`~|+-\/ '''
+SPACE = '\x0c\n\r\t\x0b\xa0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u200a\u2028\u2029\u202f\u205f\u3000\ufeff'
